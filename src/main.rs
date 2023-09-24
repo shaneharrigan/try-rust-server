a5d6932d4710d5dabe53d05ba8aa1df9bc8a7413
shaneharrigan <harrigan-s@outlook.com>
2023-09-24 20:54:46 +0100

    added custom action to route

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[get("/test")]
+fn testing_path() -> &'static str{
+    "Wow, it worked!"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index, testing_path])
 }
